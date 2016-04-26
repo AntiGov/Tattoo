72e7e8c0ab0fa1e55d522dd4482d6da8322bb824
Jonathan Coolen <jonathanrcoolen@gmail.com>
2016-04-25 19:21:11 -0500

    Added UserForms

--- collections/userForms.js
+++ collections/userForms.js
@@ -17,9 +17,46 @@ UserFormPage2 = new SimpleSchema({
 		type: String,
 		label: "Last Name"
 	},
-	/*recieveNewsLetter: {
+	recieveNewsLetter: {
+		type: Boolean.
+		label: "Would you like to receive our Enewsletter?"
 
-	}*/
+	},
+	email: {
+		type: String,
+		label: "Email"
+	}
+
 })
 
-UserForms.attachSchema([UserFormPage1, UserFormPage2]);
+UserFormPage3 = new SimpleSchema({
+	zip: {
+		type: Number,
+		label: "Zip"
+		regEx: SimpleSchema.RegEx.ZipCode
+	}
+})
+
+UserFormPage4 = new SimpleSchema({
+	brochures: {
+		type: [String],
+		label: "Brochures"
+	}
+
+	interests: {
+		type: [String],
+		label: "Interests"	
+	}
+})
+
+UserFormPage5 = new SimpleSchema({
+	comments: {
+		type: String,
+		label: "Comments"
+	}
+})
+
+
+
+
+UserForms.attachSchema([UserFormPage1, UserFormPage2, UserFormPage3]);
