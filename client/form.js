5670ba14b7cc39a386a743cc07d5870548993f08
neobii <Iwilldevelopcodeforyou@gmail.com>
2016-04-26 20:33:04 -0400

    fixed form

--- client/form.js
+++ client/form.js
@@ -1,13 +1,15 @@
-Template.form.helpers({
+Template.formView.helpers({
   steps: function() {
     return [{
       id: 'contact-information',
       title: 'How many in your party',
-      schema: UserFormPage1
+      schema: UserFormPage1,
+      formId: "insert-contact"
     }, {
       id: 'payment-information',
       title: 'subscribe',
-      schema: UserFormPage2
+      schema: UserFormPage2,
+      formId: "insrt subscrib"
     }
     ];
   }
