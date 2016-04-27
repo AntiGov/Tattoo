fe4fdfd687056d57d3e0c2298bcc69e02c2bc9d9
neobii <Iwilldevelopcodeforyou@gmail.com>
2016-04-26 20:29:09 -0400

    right place

--- client/wizard.js
+++ client/wizard.js
@@ -1,17 +1,4 @@
-Template.wizard.helpers({
-  steps: function() {
-    return [{
-      id: 'contact-information',
-      title: 'How many in your party',
-      schema: UserFormPage1
-    }, {
-      id: 'payment-information',
-      title: 'subscribe',
-      schema: UserFormPage2
-    }
-    ];
-  }
-});
+
 
 /*
 onSubmit: function(data, wizard) {
