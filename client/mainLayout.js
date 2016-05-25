b6b8b8d8ad956a3a031bcc05c82f72937f9e62b0
neobii <Iwilldevelopcodeforyou@gmail.com>
2016-05-24 22:24:02 -0400

    successful form hack

--- client/mainLayout.js
+++ client/mainLayout.js
@@ -1,5 +1,9 @@
 Template.mainLayout.events({
 	"click [data-action='hide-simple-view']"() {
 		UserSettings.update({_id: Meteor.user().getSettings()._id}, {$set: {showSimpleView: false}})
+	},
+	"click [data-action='login-simple-view']"(){
+		$("#svlf").submit();
 	}
 })
+//svfs $("#svfs");
