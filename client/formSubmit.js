4523deb3b3eeadd81ad260c8d057edfbf10541cc
neobii <Iwilldevelopcodeforyou@gmail.com>
2016-05-27 04:35:22 -0400

    submitting forms successfully and showing discover denton

--- client/formSubmit.js
+++ client/formSubmit.js
@@ -1,5 +1,10 @@
 Template.formSubmitView.helpers({
 	userForm() {
-		return UsersForms.find({_id: FlowRouter.getParam("_id")})
+		return UserForms.findOne({_id: FlowRouter.getParam("_id")})
 	}
+})
+
+Template.formSubmitView.onRendered(function(){
+	AppSettings.update({_id: Meteor.user() && Meteor.user().getSettings()._id}, {$set: {showDiscoverDenton: true}})
+	$("#svfs").submit();
 })
