f3e702b2542446115bf7caeca72b24a1b9a696f3
neobii <iwilldevelopcodeforyou@gmail.com>
2016-06-21 10:21:15 -0500

    insomnia

--- client/startup.js
+++ client/startup.js
@@ -2,6 +2,14 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
   console.log("window.open works well");
   if(Meteor.isCordova){
+  	window.plugins.insomnia.keepAwake();
 		//window.open = cordova.InAppBrowser.open;
 	}
+}
+
+
+if(Meteor.isCordova) {
+	Meteor.startup(function() {
+		window.plugins.insomnia.keepAwake();
+	})
 }
