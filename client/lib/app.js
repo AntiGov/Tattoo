bee40da2ff19966fd0a34ef78fb93f237c3ed13d
neobii <iwilldevelopcodeforyou@gmail.com>
2016-06-21 06:29:35 -0500

    moved back to server

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,5 +1,12 @@
 App = {};
-App.refreshDiscoverDenton = function(){
+App.refreshDiscoverDenton = function() {
 	App.ref.close();
 	App.ref = window.open('http://discoverdenton.com', '_blank', 'location=no,hidden=yes,toolbar=no');
 }
+
+App.hexToRgb = function(h) {
+  var r = parseInt((cutHex(h)).substring(0,2),16), g = ((cutHex(h)).substring(2,4),16), b = parseInt((cutHex(h)).substring(4,6),16)
+  return [r,g,b];
+}
+
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
