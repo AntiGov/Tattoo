8665965d94111604ca113dca2397bce48635d390
neobii <iwilldevelopcodeforyou@gmail.com>
2016-06-21 09:21:54 -0500

    killed all browser related code

--- client/discoverDenton.js
+++ client/discoverDenton.js
@@ -1,8 +1,8 @@
 Template.discoverDenton.onRendered(function(){
-	App.ref = window.open('http://discoverdenton.com', '_blank', 'location=no,hidden=yes,toolbar=no');
-	App.ref.addEventListener("loadstart", function(e){
+	//App.ref = window.open('http://discoverdenton.com', '_blank', 'location=no,hidden=yes,toolbar=no');
+	/*App.ref.addEventListener("loadstart", function(e){
 		if(UserStatus.isMonitoring()) {
 			UserStatus.pingMonitor();
 		}
-	})
+	})*/
 })
--- client/startup.js
+++ client/startup.js
@@ -2,6 +2,6 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
   console.log("window.open works well");
   if(Meteor.isCordova){
-		window.open = cordova.InAppBrowser.open;
+		//window.open = cordova.InAppBrowser.open;
 	}
 }
