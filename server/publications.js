cf11dff062c7d9985224100e2e050c19923eac96
neobii <iwilldevelopcodeforyou@gmail.com>
2016-06-24 07:47:56 -0500

    added publications

---
+++ server/publications.js
@@ -0,0 +1,32 @@
+Meteor.publish(null, function(){
+	return Images.find({});
+})
+
+Meteor.publish(null, function(){
+	if(!this.userId) {
+		return this.ready();
+	}
+	return AppSettings.find({userId: this.userId});
+})
+
+Meteor.publish(null, function(){
+	if(!this.userId){
+		return this.ready();
+	}
+	var settings = Meteor.users.findOne({_id: this.userId}).getSettings();
+	if(!settings) {
+		return this.ready();
+	}
+	return AppLocations.find({_id: settings.appLocationId});
+})
+
+Meteor.publish(null, function(){
+	if(!this.userId){
+		return this.ready();
+	}
+	var settings = Meteor.users.findOne({_id: this.userId}).getSettings();
+	if(!settings) {
+		return this.ready();
+	}
+	return AppStyles.find({_id: settings.appStyleId});
+})
