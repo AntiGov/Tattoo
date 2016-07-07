fed6755856c0f3bf59a6cf7c5499a7174523d6d6
neobii <iwilldevelopcodeforyou@gmail.com>
2016-07-07 12:21:16 -0500

    fixed login

--- client/simpleView/simpleViewIframe.js
+++ client/simpleView/simpleViewIframe.js
@@ -1,6 +1,6 @@
 Template.simpleViewIframe.events({
 	"click [data-action='login-simple-view']"() {
-		$("#svlf" + Template.parentData(1)._id).submit();
+		document.getElementById("svlf" + Template.parentData(1)._id).submit();
 	},
 	"click [data-action='goto-form']"(){
 		document.getElementById("simpleview-iframe" + Template.parentData(1)._id).src = "http://denton.simpleviewcrm.com/forms/index.cfm?action=form&formID=" + this.formId;
