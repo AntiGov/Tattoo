df3ace97bf0bfe6adb5d033d12b01f669a674ed7
neobii <iwilldevelopcodeforyou@gmail.com>
2016-07-08 15:11:05 -0500

    hopeful browser fixes

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,7 +1,7 @@
 App = {};
 App.refreshDiscoverDenton = function() {
 	//App.ref.close();
-	//App.ref = window.open('http://discoverdenton.com', '_blank', 'location=no,hidden=yes,toolbar=no');
+	//
 }
 
 App.hexToRgb = function(hex){
--- client/startup.js
+++ client/startup.js
@@ -2,6 +2,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
   if(Meteor.isCordova){
 		window.open = cordova.InAppBrowser.open;
+		App.ref = window.open('http://discoverdenton.com', '_blank', 'location=no,hidden=yes,toolbar=no');
 	}
 }
 
