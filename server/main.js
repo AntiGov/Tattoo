c0ebd6c132c275eaa0fab7f4cf77db97b7135b18
root <root@localhost.localdomain>
2016-07-19 14:21:36 -0400

    another commit

--- server/main.js
+++ server/main.js
@@ -3,3 +3,15 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.publish('studiosartists',function(){
+         return StudioArtists.find({});
+})
+
+Meteor.publish('tattoostudio',function(){
+         return TattooStudio.find({});
+})
+
+Meteor.publish('tattooappointment',function(){
+         return TattooAppointment.find({});
+})
