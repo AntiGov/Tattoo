235bda2d16c64d560d297c1aeb3e83fc7df13c57
root <root@localhost.localdomain>
2016-07-20 06:59:48 -0400

    fix admin

--- server/main.js
+++ server/main.js
@@ -1,8 +1,3 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
 
 Meteor.publish('studiosartists',function(){
          return StudioArtists.find({});
